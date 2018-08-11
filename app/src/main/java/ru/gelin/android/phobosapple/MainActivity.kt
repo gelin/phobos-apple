9b47e73b9541d8de2466817352d0f94a6c423f07
Denis Nelubin <dnelubin@gmail.com>
2018-08-11 20:02:35 +0600

    Implemented simple movie playing.

--- app/src/main/java/ru/gelin/android/phobosapple/MainActivity.kt
+++ app/src/main/java/ru/gelin/android/phobosapple/MainActivity.kt
@@ -2,13 +2,20 @@ package ru.gelin.android.phobosapple
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.VideoView
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.find
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 class MainActivity : Activity() {
+
+    private val log = AnkoLogger(javaClass)
 
     private val movies = MoviesRepository()
 
@@ -17,23 +24,27 @@ class MainActivity : Activity() {
 
         setContentView(R.layout.activity_main)
 
-//        fullscreen_content.setOnClickListener { toggle() }
+        find<View>(R.id.content).setOnClickListener { playNextMovie() }
     }
 
     override fun onResume() {
         super.onResume()
 
-        val text = StringBuilder()
-        for (i in 0 until 10) {
-            val movie = movies.nextMovie() ?: continue
-            text.append(movie.location)
-            text.append("\n")
-            text.append(movie.url)
-            text.append("\n")
+        playNextMovie()
+    }
+
+    private fun playNextMovie() {
+        val movie = movies.nextMovie()
+        if (movie == null) {
+            toast("No movies loaded")
+            return
         }
 
-        val moviesList = findViewById<TextView>(R.id.movie_list)
-        moviesList.text = text
+        log.info { "Playing $movie" }
+
+        val videoView = find<VideoView>(R.id.video)
+        videoView.setVideoPath(movie.url)
+        videoView.start()
     }
 
 }
